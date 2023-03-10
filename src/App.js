4904934e275fdd745590845c160839c1831675dd
Mohd Musarraf <musharrafpanwar@gmail.com>
2023-03-10 08:43:47 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { getDatabase, ref, set} from 'firebase/database'
 import './App.css';
+import {app} from "./firebase"
 
 function App() {
+  const db = getDatabase(app);
+  const handleClick = ()=>{
+
+    set(ref(db, "musharraf/panwar"), {
+      name: "musharraf",
+      sname: "panwar",
+      age: 21
+    })
+    alert("data successful inter")
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>this is para</p>
+      <button onClick={handleClick} >Click to insert</button>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import {initializeApp} from "firebase/app"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAEpdvkhr6EqHTv1_hiOIrUK8lj1hpizz4",
+    authDomain: "myproject-37f3a.firebaseapp.com",
+    databaseURL: "https://myproject-37f3a-default-rtdb.firebaseio.com",
+    projectId: "myproject-37f3a",
+    storageBucket: "myproject-37f3a.appspot.com",
+    messagingSenderId: "1042079597114",
+    appId: "1:1042079597114:web:c816aac826156ef8dbab03",
+    databaseURL: "https://myproject-37f3a-default-rtdb.firebaseio.com/"
+
+  };
+
+  export const app = initializeApp(firebaseConfig);
