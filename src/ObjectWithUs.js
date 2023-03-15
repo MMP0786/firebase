07bca77ebb1a8c16cd2751908cb5f17722036d01
Mohd Musarraf <musharrafpanwar@gmail.com>
2023-03-15 16:54:56 +0530

    objectWithUs

---
+++ src/ObjectWithUs.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+
+function ObjectWithUs() {
+    const obj ={
+        name: "Musharraf",
+        surname: "Panwaar",
+        gender: "Male",
+        isMarried: false
+    }
+    const [person, setPerson] = useState(obj)
+
+    const handleChange = ()=>{
+       
+    }
+  return (
+    <>
+    <div>{person.name}</div>
+    <div>{person.surname}</div>
+    <div>{person.gender}</div>
+    <div>{person.isMarried.toString()}</div>
+    
+    <button onClick={handleChange}>Update</button>
+    </>
+  )
+}
+
+export default ObjectWithUs
